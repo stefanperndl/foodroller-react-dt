4e7c87269bc460e60c22d6d7c53cc2049c618b00
zenith <pernstef@gmail.com>
2025-08-07 22:46:31 +0200

    added slug routes

--- src/app/[[...slug]]/client.js
+++ src/app/[[...slug]]/client.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const App = dynamic(() => import('../../App'));
+const App = dynamic(() => import('../../App.jsx'));
 
 export function ClientOnly() {
   return <App />;
--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { ClientOnly } from "./client"
 
 export function generateStaticParams() {
-  return [{ slug: [''] }]
+  // Add all static routes you want to export here
+  return [
+    { slug: [] }, // homepage
+    { slug: ['about'] },
+    { slug: ['contact'] },
+    // Add more as needed
+  ];
 }
  
 export default function Page() {
