61e5b1b883a9377847167123d32bb17d0786ea48
zenith <pernstef@gmail.com>
2025-10-27 19:47:23 +0100

    [next] removed unused slugs, only exporting homepage

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { ClientOnly } from "./client"
 
 export function generateStaticParams() {
-  // Add all static routes you want to export here
   return [
-    { slug: [] }, // homepage
-    { slug: ['about'] },
-    { slug: ['contact'] },
-    // Add more as needed
+    { slug: [] }
   ];
 }
  
