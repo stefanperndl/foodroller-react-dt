83bb093b8c5ade9cc83eba2781fce53a15f3319c
zenith <pernstef@gmail.com>
2025-10-27 19:48:33 +0100

    [next] fixed outputfiletracingroot dir

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
+  outputFileTracingRoot: __dirname,
 }
  
 export default nextConfig
